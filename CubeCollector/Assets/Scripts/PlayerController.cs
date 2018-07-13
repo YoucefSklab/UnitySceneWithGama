da68790c314ef3f80310e12e925b9f4ea9e0ac9c
SKLAB <ysklab@hotmail.com>
2018-07-13 11:05:54 +0200

    Add CameraController

--- CubeCollector/Assets/Scripts/PlayerController.cs
+++ CubeCollector/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
 	}
 }
