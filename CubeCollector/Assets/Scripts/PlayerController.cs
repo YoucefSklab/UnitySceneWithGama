eeac934c1f20d782638e18c646672f6d3eb7a0ff
SKLAB <ysklab@hotmail.com>
2018-07-13 13:20:14 +0200

    Display the Score and Text

--- CubeCollector/Assets/Scripts/PlayerController.cs
+++ CubeCollector/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int  count;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -25,9 +32,20 @@ public class PlayerController : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) 
 	{
-		if (other.gameObject.CompareTag ("Pick Up"))
+		if (other.gameObject.CompareTag ( "Pick Up"))
 		{
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 5)
+		{
+			winText.text = "You Win!";
 		}
 	}
 }
