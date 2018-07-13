8a4470a1e2d93a533d17ef1fa070cfbe2a51bef3
SKLAB <ysklab@hotmail.com>
2018-07-13 13:33:20 +0200

    Connect to Mqtt Server and send/receive messages

--- CubeCollector/Assets/Scripts/PlayerController.cs
+++ CubeCollector/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+
+using System;
+
+
+
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Text countText;
 	public Text winText;
+	public Text receivedMqttMessage;
 
 	private Rigidbody rb;
 	private int  count;
+
+	private MqttClient client;
+
 
 	void Start ()
 	{
@@ -18,6 +32,21 @@ public class PlayerController : MonoBehaviour {
 		count = 0;
 		SetCountText ();
 		winText.text = "";
+
+		// MQTT client Initialization 
+		// --------------------------
+
+		// create client instance 
+		//client = new MqttClient(IPAddress.Parse("143.185.118.233"),8080 , false , null ); 
+		client = new MqttClient("localhost",1883 , false , null );
+
+		// register to message received 
+		client.MqttMsgPublishReceived += client_MqttMsgPublishReceived; 
+
+		string clientId = Guid.NewGuid().ToString(); 
+		client.Connect(clientId); 
+
+		client.Subscribe(new string[] { "Unity" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
 	}
 
 	void FixedUpdate ()
@@ -28,6 +57,8 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+
+		client.MqttMsgPublishReceived += client_MqttMsgPublishReceived; 
 	}
 
 	void OnTriggerEnter(Collider other) 
@@ -37,6 +68,7 @@ public class PlayerController : MonoBehaviour {
 			other.gameObject.SetActive (false);
 			count = count + 1;
 			SetCountText ();
+			sendGotBox();
 		}
 	}
 
@@ -47,5 +79,25 @@ public class PlayerController : MonoBehaviour {
 		{
 			winText.text = "You Win!";
 		}
+	}
+
+	void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
+	{ 
+		string receivedMessage = System.Text.Encoding.UTF8.GetString (e.Message);
+		Debug.Log("Received: " +  receivedMessage );
+		Debug.Log("Good... Done!");
+		receivedMqttMessage.text = receivedMessage;
+	}
+
+	void OnGUI(){
+		if ( GUI.Button (new Rect (20,40,180,20), "Send Mqtt message")) {
+			Debug.Log("sending... SKLAB");
+			client.Publish("Gama", System.Text.Encoding.UTF8.GetBytes("Sending from Unity3D!!! Good"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+			Debug.Log("Message sent!");
+		}
+	}
+
+	void sendGotBox(){
+		client.Publish("Gama", System.Text.Encoding.UTF8.GetBytes("Great! I have got a box! Total Boxes is: "+count), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
 	}
 }
